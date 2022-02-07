5f73ca19e2a0772a147fe347244b7db4d40c0714
Эдуард Шамсиев <s-6001@yandex.ru>
2022-02-07 15:21:26 +0500

    Добавьте файлы проекта.

---
+++ Wpf_task_13/MyCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wpf_task_13
+{
+    class MyCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+        public static RoutedCommand Open { get; set; }
+        public static RoutedCommand Save { get; set; }
+        static MyCommands()
+        {
+            Exit = new RoutedCommand("Exit", typeof(MyCommands));
+            Open = new RoutedCommand("Open", typeof(MyCommands));
+            Save = new RoutedCommand("Save", typeof(MyCommands));
+        }
+    }
+}
